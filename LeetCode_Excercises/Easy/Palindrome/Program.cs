876210532ad0d190baeeddaeadb0f5a1a3f1d150
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-04-19 22:55:51 +0300

    Add files via upload

---
+++ LeetCode_Excercises/Easy/Palindrome/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            bool result = IsPalindrome(input);
+
+            Console.WriteLine(result);
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            if( x < 0)
+            {
+                return false;
+            };
+
+            char[] arr = x.ToString().ToCharArray();
+            List<char> list = new List<char>();
+            List<char> reverse = new List<char>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                list.Add(arr[i]);
+            }
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                reverse.Add(arr[i]);
+            }
+
+            bool status = true;
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (list[i] == reverse[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    status = false;
+                }
+
+            }
+            
+
+            return status;
+           
+
+            
+           
+            
+
+
+        }
+    }
+}
