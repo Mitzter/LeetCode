0aa57a0f2b987cbb5c99466b5cc20b38b3187cd5
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-04-19 11:18:35 +0300

    Add files via upload

---
+++ LeetCode_Excercises/Easy/Pivot_Index/ConsoleApp16/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = new int[] { 1, 7, 3, 6, 5, 6 };
+
+
+            int pivotIndex = PivotIndex(input);
+
+            Console.WriteLine(pivotIndex);
+
+            
+        }
+
+        public static int PivotIndex(int[] nums)
+        {
+            
+            List<int> inputList = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                inputList.Add(nums[i]);
+            }
+
+            int currentIndex = 0;
+            int leftSum;
+            int rightSum;
+            int leftmostIndex = 0;
+
+            for (int index = 0; index < inputList.Count; index++)
+            {
+                leftSum = 0;
+                rightSum = 0;
+
+                for (int i = 0; i < index; i++)
+                {
+                    leftSum += inputList[i];
+                }
+
+                for (int j = inputList.Count - 1; j > index; j--)
+                {
+                    rightSum += inputList[j];
+                }
+
+                currentIndex++;
+
+                if (leftSum == rightSum)
+                {
+                    leftmostIndex = index;
+                    break;
+                }
+
+                else if(leftSum != rightSum)
+                {
+                    leftmostIndex = -1;
+                    
+                }
+
+               
+            }
+            return leftmostIndex;
+        }
+    }
+}
