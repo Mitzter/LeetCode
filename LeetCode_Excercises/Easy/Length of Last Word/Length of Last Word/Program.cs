5d5a279312344ec93c2007cb0fd6994d7c9e9aef
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-04-22 10:00:51 +0300

    Add files via upload

---
+++ LeetCode_Excercises/Easy/Length of Last Word/Length of Last Word/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
