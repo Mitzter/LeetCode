433190f7e6088fe92992cf08b8f119bdb55c0ba2
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-04-22 09:45:59 +0300

    Add files via upload

---
+++ LeetCode_Excercises/Easy/Remove Element in Array/Remove Element in Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Remove_Element_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 2, 2, 3 };
+            int valueToRemove = 3;
+
+            int k = RemoveElement(nums, valueToRemove);
+
+            Console.WriteLine(k);
+        }
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int result = 0;
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if(nums[i] != val)
+            //    {
+            //        nums[result] = nums[i];
+            //        result++;
+            //    }
+            //}
+
+            nums = nums.Where(x => x != val).ToArray();
+
+            result = nums.Length;
+            return result;
+            
+        }
+    }
+}
