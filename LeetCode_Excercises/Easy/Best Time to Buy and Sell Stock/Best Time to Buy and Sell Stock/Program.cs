46039ca3fbfd73f37906ae81014c06652c097fab
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-04-22 12:05:08 +0300

    Add files via upload

---
+++ LeetCode_Excercises/Easy/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] prices = new int[]
+            {
+               7,1,5,3,6,4
+            };
+
+            int result = MaxProfit(prices);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            var minPrice = int.MaxValue;
+            return prices.Max(price =>
+            {
+                minPrice = Math.Min(minPrice, price);
+                return price - minPrice;
+            });
+        }
+    }
+}
