3a58d5dba062bf2b4d88feab51681d582c24545e
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-04-22 09:53:58 +0300

    Add files via upload

---
+++ LeetCode_Excercises/Easy/First Occurrence in a String/First Occurrence in a String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace First_Occurrence_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string haystack = Console.ReadLine();
+            string needle = Console.ReadLine();
+
+            Console.WriteLine(StrStr(haystack, needle));
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+    }
+}
