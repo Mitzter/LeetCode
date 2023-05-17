ed565acfe6ce2d39ee57d5cf735e64eeaf45f3b6
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-05-17 11:06:32 +0300

    Add files via upload

---
+++ LeetCode_Excercises/Medium/RemoveDuplicates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 5, 7, 7, 8, 8, 10 };
+
+            int target = 8;
+
+            Console.WriteLine("[{0}]", string.Join(", ", SearchRange(input, target)));
+            
+        }
+
+        public static int[] SearchRange(int[] nums, int target)
+        {
+            List<int> coordinates = new List<int>(2);
+
+            coordinates.Add(Array.IndexOf(nums,target));
+            coordinates.Add(Array.LastIndexOf(nums, target));
+
+
+            return coordinates.ToArray();
+        }
+    }
+}
